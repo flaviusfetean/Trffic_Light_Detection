a0947afa03209bfc0a442f7324ed8ec3ab30c15b
flaviusfetean <flavius_fetean@yahoo.com>
2022-01-04 19:31:57 +0200

    Added sorted traffic lights
    Sorted them on green/yellow/red/none and discarded those who were not traffic lights

--- image_sorter.py
+++ image_sorter.py
@@ -7,17 +7,23 @@ BASE_PATH = "traffic_light_dataset"
 color_paths = {"0": "0_green", "1": "1_yellow", "2": "2_red", "3": "3_not"}
 
 images_paths = list(list_images(PATH))
-
 images = [cv2.imread(img_path) for img_path in images_paths]
 
 
+offset = 0
+images = images[offset:]
+
 for (i, image) in enumerate(images):
-    cv2.imshow(image)
-    k = cv2.waitKey(33)
+    cpy_image = cv2.resize(image, (80, 160))
+    cv2.imshow("img", cpy_image)
+    k = cv2.waitKey(0)
     if k == ord('q'):
         print("Stopped at img number {}".format(i))
         break
-    destination_path = os.path.sep.join([BASE_PATH, color_paths[chr(k)]])
+    if k == ord('4'):
+        continue
+    destination_path = os.path.sep.join([BASE_PATH, color_paths[chr(k)], "{}.jpg".format(i + offset)])
+    cv2.imwrite(destination_path, image)
 
 print("Successfully sorted all images")
 cv2.destroyAllWindows()
