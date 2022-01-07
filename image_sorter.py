efc3942a037cb0cdc64d202d1f959000c6e28b4d
flaviusfetean <flavius_fetean@yahoo.com>
2022-01-07 02:24:41 +0200

    added series 1 of new traffic lights
    added a series of 1607 traffic lights sorted into red/yellow/green/none

--- image_sorter.py
+++ image_sorter.py
@@ -7,23 +7,30 @@ BASE_PATH = "traffic_light_dataset"
 color_paths = {"0": "0_green", "1": "1_yellow", "2": "2_red", "3": "3_not"}
 
 images_paths = list(list_images(PATH))
-images = [cv2.imread(img_path) for img_path in images_paths]
 
+f = open("info.txt", "r")
+lines = f.readlines()
+offset, num_traffic_lights = int(lines[0]), int(lines[1])
 
-offset = 0
-images = images[offset:]
+images_paths = images_paths[offset:]
 
-for (i, image) in enumerate(images):
+for (i, image_path) in enumerate(images_paths):
+    image = cv2.imread(image_path)
     cpy_image = cv2.resize(image, (80, 160))
     cv2.imshow("img", cpy_image)
     k = cv2.waitKey(0)
     if k == ord('q'):
-        print("Stopped at img number {}".format(i))
+        print("Stopped at img number {}".format(i + offset))
         break
     if k == ord('4'):
         continue
-    destination_path = os.path.sep.join([BASE_PATH, color_paths[chr(k)], "{}.jpg".format(i + offset)])
+    destination_path = os.path.sep.join([BASE_PATH, color_paths[chr(k)], "cityscapes_{}.jpg".format(i + offset)])
     cv2.imwrite(destination_path, image)
+    num_traffic_lights += 1
+    print("{} images added".format(num_traffic_lights))
 
-print("Successfully sorted all images")
+    f = open("info.txt", "w")
+    f.writelines([str(i+offset) + "\n", str(num_traffic_lights)])
+    f.close()
+
 cv2.destroyAllWindows()
